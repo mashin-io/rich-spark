38bd72d5eb6404993bb8682568c7ac12097b6b07
Ahmed Mahran <ahmed.mahran@mashin.io>
2016-04-16 14:53:31 +0200

    Add RichRDDFunctions

---
+++ src/main/scala/io/mashin/rich/spark/RichRDDFunctions.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2016 Mashin (http://mashin.io). All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.mashin.rich.spark
+
+import org.apache.spark.rdd.{ScanLeftRDD, RDD}
+
+import scala.reflect.ClassTag
+
+class RichRDDFunctions[T: ClassTag](rdd: RDD[T]) extends Serializable {
+
+  def scanLeft(zero: T, init: T, f: (T, T) => T): RDD[T] = {
+    ScanLeftRDD.scanLeft[T](rdd, zero, init, f)
+  }
+
+}
---
+++ src/test/scala/io/mashin/rich/spark/RichRDDFunctionsSuite.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2016 Mashin (http://mashin.io). All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.mashin.rich.spark
+
+import io.mashin.rich.spark.RichRDD._
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest.{ShouldMatchers, FunSuite}
+
+import scala.util.Random
+
+class RichRDDFunctionsSuite extends FunSuite with ShouldMatchers {
+
+  test("Scan Left RDD") {
+    val sc = sparkContext("Scan-Left-RDD")
+
+    val parts = 4
+    val partSize = 1000
+
+    val scanLeftTest: (Array[Int], Int, (Int, Int) => Int) => Unit = {(input, init, f) =>
+      val rdd = sc.makeRDD(input, parts)
+      val rddScanned = rdd.scanLeft(0, init, f)
+      rddScanned.collect() should be (input.scanLeft(init)(f))
+    }
+
+    val f = (a: Int, b: Int) => a + b
+    (-10 to 10).foreach {i =>
+      scanLeftTest((1 to parts * partSize).map(_ => i + Random.nextInt(10)).toArray, i, f)
+    }
+
+    sc.stop()
+  }
+
+  test("Scan Left Pair RDD") {
+    val sc = sparkContext("Scan-Left-Pair-RDD")
+
+    val parts = 4
+    val partSize = 1000
+
+    val scanLeftTest: (Array[Int], Int, (Int, Int) => Int) => Unit = {(input, init, f) =>
+      val rdd = sc.makeRDD(input, parts)
+      val rddScanned = rdd.scanLeft(0, init, f)
+      rddScanned.collect() should be (input.scanLeft(init)(f))
+    }
+
+    val f = (a: Int, b: Int) => a + b
+    (-10 to 10).foreach {i =>
+      scanLeftTest((1 to parts * partSize).map(_ => i + Random.nextInt(10)).toArray, i, f)
+    }
+
+    sc.stop()
+  }
+
+  private def sparkContext(name: String): SparkContext = {
+    new SparkContext(new SparkConf().setAppName(name).setMaster("local[*]"))
+  }
+
+}
