7f809382d5a20638c022ca2e56587a83b6a649fc
Ahmed Mahran <ahmed.mahran@mashin.io>
2016-05-13 23:19:48 +0200

    Make TimerEventSource serializable

--- streaming/src/main/scala/org/apache/spark/streaming/event/TimerEventSource.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/event/TimerEventSource.scala
@@ -1,39 +1,76 @@
 
 package org.apache.spark.streaming.event
 
-import org.apache.spark.internal.Logging
-import org.apache.spark.streaming.StreamingContext
-import org.apache.spark.util.Clock
+import org.apache.spark.streaming.{Duration, StreamingContext, Time}
 
-case class TimerEvent(source: EventSource, override val time: Long, index: Long)
+case class TimerEvent(source: EventSource, override val time: Time, index: Long)
   extends Event(source)
 
 class TimerEventSource(
-    ssc: StreamingContext,
-    clock: Clock,
-    startTime: Long,
-    endTime: Long,
-    period: Long,
+    @transient ssc: StreamingContext,
+    private var startTime: Time,
+    private var endTime: Time,
+    private var period: Duration,
     name: String
-  ) extends EventSource(ssc) with Logging {
-
-  require(startTime < endTime,
-    s"End time ($endTime ms) should be greater than start time ($startTime ms).")
-
-  require(period > 0, s"Period ($period ms) should be greater than 0.")
+  ) extends EventSource(ssc) {
 
   private var stopped = false
-  private var nextTime: Long = -1
+  private var nextTime: Time = Time(-1)
 
-  private val thread = new Thread(s"TimerEventSource $name") {
-    setDaemon(true)
-    override def run() { loop() }
+  def getStartTime: Time = startTime.copy()
+
+  def getEndTime: Time = endTime.copy()
+
+  def getPeriod: Duration = period.copy()
+
+  def setStartTime(time: Time) {
+    startTime = time
+  }
+
+  def setEndTime(time: Time) {
+    endTime = time
+  }
+
+  def setPeriod(period: Duration) {
+    this.period = period
+  }
+
+  override def start(): Unit = synchronized {
+    require(startTime < endTime,
+      s"End time ($endTime ms) should be greater than start time ($startTime ms).")
+
+    require(period.milliseconds > 0, s"Period $period should be greater than 0.")
+
+    val currTime = Time(System.currentTimeMillis)
+    if (startTime < currTime) {
+      // if start time already elapsed, start from the next possible
+      // period after current time
+      nextTime = startTime + period * (1 + (currTime - startTime) / period).toInt
+    } else {
+      nextTime = startTime
+    }
+    stopped = false
+    new Thread(s"TimerEventSource $name") {
+      setDaemon(true)
+      override def run() { loop() }
+    }.start()
+    logDebug(s"Timer ($name) started, first timer event emits on $nextTime ms")
+  }
+
+  override def restart() = start()
+
+  override def stop(): Unit = synchronized {
+    if (!stopped) {
+      stopped = true
+      logDebug(s"Timer ($name) stopped")
+    }
   }
 
   private def loop(): Unit = {
+    val clock = context.clock
     var index = 0
     while (!stopped && nextTime <= endTime) {
-      clock.waitTillTime(nextTime)
+      clock.waitTillTime(nextTime.milliseconds)
       post(TimerEvent(this, nextTime, index))
       logDebug(s"Timer ($name) event with time $nextTime ms and period index $index")
       index += 1
@@ -42,23 +79,4 @@ class TimerEventSource(
     stop()
   }
 
-  override def start(): Unit = synchronized {
-    val currTime = System.currentTimeMillis
-    if (startTime < currTime) {
-      // if start time already elapsed, start from the next possible
-      // period after current time
-      nextTime = startTime + period * (1 + (currTime - startTime) / period)
-    } else {
-      nextTime = startTime
-    }
-    thread.start()
-    logDebug(s"Timer ($name) started, next time $nextTime ms")
-  }
-
-  override def stop(): Unit = synchronized {
-    if (!stopped) {
-      stopped = true
-      logDebug(s"Timer ($name) stopped")
-    }
-  }
 }
