633d3a330b0e7762c574e962878a7241191a5829
Ahmed Mahran <ahmed.mahran@mashin.io>
2016-05-18 00:40:44 +0200

    Make MappedDStream event based

--- streaming/src/main/scala/org/apache/spark/streaming/dstream/MappedDStream.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/dstream/MappedDStream.scala
@@ -17,10 +17,11 @@
 
 package org.apache.spark.streaming.dstream
 
-import scala.reflect.ClassTag
-
 import org.apache.spark.rdd.RDD
-import org.apache.spark.streaming.{Duration, Time}
+import org.apache.spark.streaming.event.Event
+import org.apache.spark.streaming.{Dependency, Duration, EventDependency}
+
+import scala.reflect.ClassTag
 
 private[streaming]
 class MappedDStream[T: ClassTag, U: ClassTag] (
@@ -28,12 +29,13 @@ class MappedDStream[T: ClassTag, U: ClassTag] (
     mapFunc: T => U
   ) extends DStream[U](parent.ssc) {
 
-  override def dependencies: List[DStream[_]] = List(parent)
+  override def dependencies: List[Dependency[_]] = List(new EventDependency[T](parent))
 
   override def slideDuration: Duration = parent.slideDuration
 
-  override def compute(validTime: Time): Option[RDD[U]] = {
-    parent.getOrCompute(validTime).map(_.map[U](mapFunc))
+  override def compute(event: Event): Option[RDD[U]] = {
+    dependencies.flatMap(_.rdds(event)).headOption
+      .map(_.asInstanceOf[RDD[T]].map[U](mapFunc))
   }
 }
 
