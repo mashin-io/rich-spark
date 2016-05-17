42e571dc18860ebc440d9277e80dfa0e831e47ca
Ahmed Mahran <ahmed.mahran@mashin.io>
2016-05-18 01:21:07 +0200

    Make DStreamGraph event based

--- streaming/src/main/scala/org/apache/spark/streaming/DStreamGraph.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/DStreamGraph.scala
@@ -18,18 +18,24 @@
 package org.apache.spark.streaming
 
 import java.io.{IOException, ObjectInputStream, ObjectOutputStream}
-
-import scala.collection.mutable.ArrayBuffer
+import java.util.concurrent.ConcurrentHashMap
 
 import org.apache.spark.internal.Logging
 import org.apache.spark.streaming.dstream.{DStream, InputDStream, ReceiverInputDStream}
+import org.apache.spark.streaming.event.{Event, EventSource, TimerEventSource}
 import org.apache.spark.streaming.scheduler.Job
 import org.apache.spark.util.Utils
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
 final private[streaming] class DStreamGraph extends Serializable with Logging {
 
   private val inputStreams = new ArrayBuffer[InputDStream[_]]()
   private val outputStreams = new ArrayBuffer[DStream[_]]()
+
+  val eventSourceToBoundStreams = new ConcurrentHashMap[EventSource, mutable.HashSet[DStream[_]]]
 
   var rememberDuration: Duration = null
   var checkpointInProgress = false
@@ -37,40 +43,55 @@ final private[streaming] class DStreamGraph extends Serializable with Logging {
   var zeroTime: Time = null
   var startTime: Time = null
   var batchDuration: Duration = null
+  var defaultTimer: TimerEventSource = null
+
+  def eventSources = {
+    eventSourceToBoundStreams.keySet().toSet
+  }
 
   def start(time: Time) {
     this.synchronized {
       require(zeroTime == null, "DStream graph computation already started")
       zeroTime = time
       startTime = time
+      defaultTimer.setStartTime(time + batchDuration)
       outputStreams.foreach(_.initialize(zeroTime))
       outputStreams.foreach(_.remember(rememberDuration))
       outputStreams.foreach(_.validateAtStart)
+      outputStreams.filter(_.boundEventSources.isEmpty)
+        .foreach(_.bind(defaultTimer))
       inputStreams.par.foreach(_.start())
+      eventSources.par.foreach(_.start())
     }
   }
 
   def restart(time: Time) {
-    this.synchronized { startTime = time }
+    this.synchronized {
+      startTime = time
+      eventSources.par.foreach(_.restart())
+    }
   }
 
   def stop() {
     this.synchronized {
+      eventSources.par.foreach(_.stop())
       inputStreams.par.foreach(_.stop())
     }
   }
 
   def setContext(ssc: StreamingContext) {
     this.synchronized {
+      eventSources.foreach(_.setContext(ssc))
       outputStreams.foreach(_.setContext(ssc))
     }
   }
 
-  def setBatchDuration(duration: Duration) {
+  def setBatchDuration(duration: Duration, ssc: StreamingContext) {
     this.synchronized {
       require(batchDuration == null,
         s"Batch duration already set as $batchDuration. Cannot set it again.")
       batchDuration = duration
+      defaultTimer = ssc.timer(Time(0), Time(Long.MaxValue), batchDuration, "DefaultTimer")
     }
   }
 
@@ -96,6 +117,16 @@ final private[streaming] class DStreamGraph extends Serializable with Logging {
     }
   }
 
+  def bind(stream: DStream[_], eventSource: EventSource) {
+    val boundStreams = Option(eventSourceToBoundStreams.get(eventSource))
+      .getOrElse {
+        val emptySet = mutable.HashSet.empty[DStream[_]]
+        eventSourceToBoundStreams.put(eventSource, emptySet)
+        emptySet
+      }
+    boundStreams += stream
+  }
+
   def getInputStreams(): Array[InputDStream[_]] = this.synchronized { inputStreams.toArray }
 
   def getOutputStreams(): Array[DStream[_]] = this.synchronized { outputStreams.toArray }
@@ -106,45 +137,50 @@ final private[streaming] class DStreamGraph extends Serializable with Logging {
       .toArray
   }
 
+  def getBoundStreams(eventSource: EventSource): Seq[DStream[_]] = {
+    eventSourceToBoundStreams.getOrDefault(eventSource, mutable.HashSet.empty).toSeq
+  }
+
   def getInputStreamName(streamId: Int): Option[String] = synchronized {
     inputStreams.find(_.id == streamId).map(_.name)
   }
 
-  def generateJobs(time: Time): Seq[Job] = {
-    logDebug("Generating jobs for time " + time)
+  def generateJobs(event: Event): Seq[Job] = {
+    logDebug(s"Generating jobs for event $event")
     val jobs = this.synchronized {
-      outputStreams.flatMap { outputStream =>
-        val jobOption = outputStream.generateJob(time)
-        jobOption.foreach(_.setCallSite(outputStream.creationSite))
-        jobOption
-      }
+      getBoundStreams(event.eventSource)
+        .flatMap { stream =>
+          val jobOption = stream.generateJob(event)
+          jobOption.foreach(_.setCallSite(stream.creationSite))
+          jobOption
+        }
     }
-    logDebug("Generated " + jobs.length + " jobs for time " + time)
+    logDebug(s"Generated ${jobs.length} jobs for event $event")
     jobs
   }
 
-  def clearMetadata(time: Time) {
-    logDebug("Clearing metadata for time " + time)
+  def clearMetadata(event: Event) {
+    logDebug("Clearing metadata for event " + event)
     this.synchronized {
-      outputStreams.foreach(_.clearMetadata(time))
+      outputStreams.foreach(_.clearMetadata(event))
     }
-    logDebug("Cleared old metadata for time " + time)
+    logDebug("Cleared old metadata for event " + event)
   }
 
-  def updateCheckpointData(time: Time) {
-    logInfo("Updating checkpoint data for time " + time)
+  def updateCheckpointData(event: Event) {
+    logInfo("Updating checkpoint data for event " + event)
     this.synchronized {
-      outputStreams.foreach(_.updateCheckpointData(time))
+      outputStreams.foreach(_.updateCheckpointData(event))
     }
-    logInfo("Updated checkpoint data for time " + time)
+    logInfo("Updated checkpoint data for event " + event)
   }
 
-  def clearCheckpointData(time: Time) {
-    logInfo("Clearing checkpoint data for time " + time)
+  def clearCheckpointData(event: Event) {
+    logInfo("Clearing checkpoint data for event " + event)
     this.synchronized {
-      outputStreams.foreach(_.clearCheckpointData(time))
+      outputStreams.foreach(_.clearCheckpointData(event))
     }
-    logInfo("Cleared checkpoint data for time " + time)
+    logInfo("Cleared checkpoint data for event " + event)
   }
 
   def restoreCheckpointData() {
