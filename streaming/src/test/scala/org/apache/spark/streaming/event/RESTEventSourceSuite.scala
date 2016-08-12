9bf4d430e233d0b23d932216e7678029034bfb94
Ahmed Mahran <ahmed.mahran@mashin.io>
2016-08-12 23:09:13 +0200

    Add test suite for REST event source

---
+++ streaming/src/test/scala/org/apache/spark/streaming/event/RESTEventSourceSuite.scala
@@ -0,0 +1,94 @@
+package org.apache.spark.streaming.event
+
+import java.net.{HttpURLConnection, URL}
+import java.util.concurrent.atomic.AtomicInteger
+
+import org.scalatest.concurrent.Eventually._
+import spark.utils.IOUtils
+
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.{Duration, StreamingContext, TestSuiteBase}
+
+class RESTEventSourceSuite extends TestSuiteBase {
+
+  val host = "localhost"
+  val port = 4140
+  val amountParam = "amount"
+  val pathCounterAdd = "/counter/add"
+  val pathCounterSub = "/counter/sub"
+
+  def setupContext(appName: String, batchDuration: Duration): StreamingContext = {
+    val conf = new SparkConf().setMaster("local[2]").setAppName(appName)
+    new StreamingContext(conf, batchDuration)
+  }
+
+  test("REST event source") {
+    withStreamingContext(setupContext("REST event source", Duration(100))) { ssc =>
+
+      val counter = new AtomicInteger(0)
+
+      val restServer = ssc.restServer(host, port, "restServer")
+        .get(pathCounterAdd).get(pathCounterSub)
+
+      ssc.textFileStream("/tmp")
+        .foreachRDD { (rdd: RDD[String], event: Event) =>
+          event match {
+            case RESTEvent(_, request: RESTRequest, _, _) =>
+              assert(request.queryMap.contains(amountParam),
+                s"Request missing $amountParam parameter")
+
+              val amount = request.queryMap(amountParam)(0).toInt
+              if (request.pathInfo.equals(pathCounterAdd)) {
+                counter.addAndGet(amount)
+              } else if (request.pathInfo.equals(pathCounterSub)) {
+                counter.addAndGet(-amount)
+              }
+
+            case _ =>
+              fail(s"Did not expect event $event")
+          }
+          ()
+        }
+        .bind(restServer)
+
+      ssc.start()
+
+      // wait enough time for the rest server to be started
+      Thread.sleep(1000)
+
+      (1 to 10).foreach { i =>
+        val (status, _) = request("GET", s"$pathCounterAdd?$amountParam=$i")
+        assert(status == 200)
+        Thread.sleep(100)
+      }
+
+      eventually(eventuallyTimeout) {
+        assert(counter.get == (1 to 10).sum)
+      }
+
+      (1 to 10).foreach { i =>
+        val (status, _) = request("GET", s"$pathCounterSub?$amountParam=$i")
+        assert(status == 200)
+        Thread.sleep(100)
+      }
+
+      eventually(eventuallyTimeout) {
+        assert(counter.get == 0)
+      }
+
+    }
+  }
+
+  def request(method: String, path: String): (Int, String) = {
+    val url = new URL(s"http://$host:$port" + path)
+    val connection = url.openConnection().asInstanceOf[HttpURLConnection]
+    connection.setRequestMethod(method)
+    connection.setDoOutput(true)
+    connection.connect()
+    val body = IOUtils.toString(connection.getInputStream)
+    connection.disconnect()
+    (connection.getResponseCode, body)
+  }
+
+}
